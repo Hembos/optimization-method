3cec374a6bb1ce763df5a873b4ea483b4cf32595
Hembos <faizrahmanov.artur@mail.ru>
2022-03-16 13:52:29 +0300

    Ended potential method

--- Transport task/PotentialMethod.py
+++ Transport task/PotentialMethod.py
@@ -11,17 +11,13 @@ def get_solution(transport_task, storage_points, destinations, rows_num, cols_nu
         potential_storage, potential_destinations, approach, rows_num, cols_num, transport)
 
     while is_optimal != True:
-        print("Potential storage:", potential_storage)
-        print("Potential destination", potential_destinations)
-        print("Approach", approach)
         calculate_next_approach(approach=approach, point=point)
         potential_storage, potential_destinations = get_potentials(
             transport_task=transport_task, approach=approach, rows_num=rows_num, cols_num=cols_num)
         is_optimal, point = is_it_optimal_plan(
             potential_storage, potential_destinations, approach, rows_num, cols_num, transport)
-
-    solution = sum(transport_task[approach[key][0]]
-                   [approach[key][1]] * approach[key] for key in approach)
+        
+    solution = sum(transport_task[key[0]][key[1]] * approach[key] for key in approach)
 
     return solution
 
@@ -36,27 +32,33 @@ def is_it_optimal_plan(potential_storage, potential_destinations, approach, rows
 
     return True, point
 
+def recursive_potentials_calc(potential_storage, potential_destinations, transport_task, visit_graph, cur_point):
+    for x in visit_graph[cur_point]:
+        if potential_storage[x[0]] is None and potential_destinations[x[1]] is not None:
+            potential_storage[x[0]] = potential_destinations[x[1]] - transport_task[x[0]][x[1]]
+            recursive_potentials_calc(potential_storage, potential_destinations, transport_task, visit_graph, x)
+        elif potential_destinations[x[1]] is None and potential_storage[x[0]] is not None:
+            potential_destinations[x[1]] = transport_task[x[0]][x[1]] + potential_storage[x[0]]
+            recursive_potentials_calc(potential_storage, potential_destinations, transport_task, visit_graph, x)
+    
 
 def get_potentials(transport_task, approach, rows_num, cols_num):
-    potential_storage = [0 for i in range(rows_num)]
-    potential_destinations = [0 for i in range(cols_num)]
+    potential_storage = [None for i in range(rows_num)]
+    potential_destinations = [None for i in range(cols_num)]
 
-    storage_index = 0
-    destinations_index = 0
-
+    key = list(approach.keys())[0]
+    potential_storage[key[0]] = 0
+    potential_destinations[key[1]] = transport_task[key[0]][key[1]]
+    
+    visit_graph = {}
     for x in approach:
-        if x[0] <= storage_index:
-            destinations_index = x[1]
-            potential_destinations[destinations_index] = transport_task[storage_index][destinations_index] - \
-                potential_storage[storage_index]
-        else:
-            storage_index = x[0]
-            potential_storage[storage_index] = transport_task[storage_index][destinations_index] - \
-                potential_destinations[destinations_index]
-            destinations_index = x[1]
-            potential_destinations[destinations_index] = transport[storage_index][destinations_index] - \
-                potential_storage[storage_index]
-
+        visit_graph[x] = []
+        for y in approach:
+            if x[0] == y[0] and x[1] != y[1] or x[1] == y[1] and x[0] != y[0]:
+                visit_graph[x].append(y)
+    
+    recursive_potentials_calc(potential_storage, potential_destinations, transport_task, visit_graph, key)
+    
     return potential_storage, potential_destinations
 
 
@@ -133,19 +135,4 @@ if __name__ == "__main__":
     storage_points = [19, 5, 21, 9]
     destinations = [12, 12, 11, 8, 11]
 
-    appr = {
-        (0, 0): 12,
-        (0, 1): 7,
-        (1, 1): 5,
-        (1, 2): 0,
-        (2, 2): 11,
-        (2, 3): 8,
-        (2, 4): 2,
-        (3, 4): 9
-    }
-
-    pot_storage, pot_dest = get_potentials(transport, appr, 4, 5)
-
-    #print(calculate_next_approach(appr, (3, 0), 4, 5))
-    #print(is_it_optimal_plan(pot_storage, pot_dest, appr, 4, 5, transport))
     print(get_solution(transport, storage_points, destinations, 4, 5))
