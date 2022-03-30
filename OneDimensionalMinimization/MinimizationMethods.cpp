e0d9b4060ec2620f6cbfdf63ddd04122fb1db092
Hembos <faizrahmanov.artur@mail.ru>
2022-03-31 00:43:55 +0300

    implemented golden section method

--- OneDimensionalMinimization/MinimizationMethods.cpp
+++ OneDimensionalMinimization/MinimizationMethods.cpp
@@ -7,7 +7,7 @@ double MinimizationMethods::dichotomyMethod(double epsilon)
     double b = targetFunction.getRightBorder();
 
     while (b - a > epsilon)
-    {   
+    {
         double delta = 0.01 * (b - a);
         double x1 = (a + b) / 2 - delta;
         double x2 = (a + b) / 2 + delta;
@@ -29,6 +29,31 @@ double MinimizationMethods::dichotomyMethod(double epsilon)
 
 double MinimizationMethods::goldenSectionMethod(double epsilon)
 {
+    double a = targetFunction.getLeftBorder();
+    double b = targetFunction.getRightBorder();
+    double alpha = (3 - sqrt(5)) / 2;
 
-    return 0;
+    double lambda = a + alpha * (b - a);
+    double mu = a + (1 - alpha) * (b - a);
+
+    while (b - a > epsilon)
+    {
+        double fLambda = targetFunction(lambda);
+        double fMu = targetFunction(mu);
+
+        if (fLambda <= fMu)
+        {
+            b = mu;
+            mu = lambda;
+            lambda = a + alpha * (b - a);
+        }
+        else
+        {
+            a = lambda;
+            lambda = mu;
+            mu = a + (1 - alpha) * (b - a);
+        }
+    }
+
+    return (a + b) / 2;
 }
--- OneDimensionalMinimization/main.cpp
+++ OneDimensionalMinimization/main.cpp
@@ -7,7 +7,8 @@ int main()
     Function function = Function([](double x){ return sin(x) * x + 2 * cos(x); }, -5, -4);
     MinimizationMethods methods = MinimizationMethods(function);
     
-    std::cout << methods.dichotomyMethod(0.02) << std::endl;
+    std::cout << methods.dichotomyMethod(0.00001) << std::endl;
+    std::cout << methods.goldenSectionMethod(0.00001) << std::endl;
 
     return 0;
 }
