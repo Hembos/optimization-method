e6af8a62ed62c906e7bcbfe7e9277585ad1c1dba
MordovinNik <mordovin_nikolya@mail.ru>
2022-04-18 19:11:54 +0300

    changed to bfgs, added logger

--- MultidimensionalMinimization/secondOrderGradientMethod.py
+++ MultidimensionalMinimization/secondOrderGradientMethod.py
@@ -30,14 +30,27 @@ def find_alpha(x_k: np.array, p_k: np.array, eps: float, max_step: float):
 
 
 def bfgs_solve(x_0: list, eps: float, max_step: float, n: int):
+    """
+
+    Args:
+        x_0: начальная точка
+        eps: точность
+        max_step: максимальный шаг
+        n: частота сброса Ak
+
+    Returns: точка минимума, значение функции в точке минимума, количество итераций
+
+    """
     xk = np.array(x_0)
     N = len(xk)
     I = np.eye(N, dtype=float)
     Ak = I
     omega = -f_grad(xk)
     iteration = 0
-
-    while la.norm(omega) > eps:
+    file = open('secondOrderGradientMethod.log', 'w')
+    while True:
+        file.write("iteration: " + str(iteration) + '\n' + "x_k: [" + str(xk.item(0)) + "," + str(
+            xk.item(1)) + "]\nf(X_k) = " + str(f(xk)) + "\n")
         pk = np.dot(Ak, omega)
         alpha_k = find_alpha(xk, pk, eps, max_step)
         xk_new = xk + alpha_k * pk
@@ -47,16 +60,20 @@ def bfgs_solve(x_0: list, eps: float, max_step: float, n: int):
         dAk1 = (dxk * np.transpose(dxk)).item(0) / (np.transpose(omega) * dxk).item(0)
         dAk2 = (Ak * domega * np.transpose(domega) * np.transpose(Ak)).item(0) / (
                 np.transpose(domega) * Ak * domega).item(0)
-        Ak_new = Ak - dAk1 - dAk2
+        rk = (Ak * domega) / (np.transpose(domega) * Ak * domega).item(0) - dxk / (np.transpose(dxk) * domega).item(0)
+        dAk3 = np.transpose(domega) * Ak * domega * rk
+        Ak_new = Ak + dAk1 + dAk2 - dAk3
         Ak = Ak_new
         xk = xk_new
         omega = omega_new
+        if la.norm(omega) < eps:
+            break
         iteration += 1
         if iteration % n == 0:
             Ak = I
-
+    file.close()
     return xk, f(xk), iteration
 
 
 if __name__ == '__main__':
-    print(bfgs_solve([100, 0], 1e-1, 10, 1))
+    print(bfgs_solve([0, 0], 1e-6, 10, 10))
